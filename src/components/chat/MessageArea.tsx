1132d6a6bd4be299bedcaf069c86eb2def2af72c
Alva <asjun2lin@outlook.com>
2023-05-11 11:06:26 +0800

    feat: 发送消息后自动滚动到最低部

--- src/components/chat/MessageArea.tsx
+++ src/components/chat/MessageArea.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import useChatHub from "../../hooks/useChatHub";
 import { ChatId } from "../../services/chat/models";
 import MessageItem from "./MessageItem";
@@ -14,6 +14,10 @@ const MessageArea = ({ chatId }: MessageAreaProps) => {
 
   const { onSend, messages, isLoading, refetch, restore } = useChatHub({ chatId });
 
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const scrollAnchorRef = useRef<HTMLDivElement>(null);
+  const [shouldScroll, setShouldScroll] = useState(true);
+
   useEffect(() => {
     if (chatId) {
         refetch(chatId);
@@ -22,17 +26,37 @@ const MessageArea = ({ chatId }: MessageAreaProps) => {
     }
   }, [chatId, dispatch, refetch, restore]);
 
+  useEffect(() => {
+    if (scrollAnchorRef.current && shouldScroll) {
+        scrollAnchorRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    }
+  }, [shouldScroll, messages])
+
+  const handleScroll = () => {
+    if (scrollContainerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = scrollContainerRef.current;
+      const isScrolledToBottom = Math.abs(scrollHeight - scrollTop - clientHeight) < 100;
+      setShouldScroll(isScrolledToBottom);
+    }
+  };
+
+  const myOnSend = useCallback((text: string) => {
+    setShouldScroll(true);
+    onSend(text);
+  }, [onSend]);
+
   return (
     <>
-      <div
-        className={"w-full h-full flex flex-col justify-start overflow-y-auto"}
-      >
-        {messages.map((message, index) => (
-          <MessageItem key={message.id} message={message} index={index} />
-        ))}
-        <div className={"flex-none h-48"}></div>
+      <div className="h-full w-full flex flex-col relative overflow-y-scroll"
+       ref={scrollContainerRef} onScroll={handleScroll}>
+        <div className={"w-full flex-1 flex flex-col justify-start"}>
+          {messages.map((message, index) => (
+            <MessageItem key={message.id} message={message} index={index} />
+          ))}
+          <div className={"flex-none h-32"} ref={scrollAnchorRef}></div>
+        </div>
       </div>
-      <SendArea onSend={onSend} disabled={isLoading} />
+      <SendArea onSend={myOnSend} disabled={isLoading} />
     </>
   );
 };
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,14 +3,14 @@ import Footer from "../components/Footer.tsx";
 import Menu from "../components/menu/Menu.tsx";
 
 const HomeComponent = () => {
-  
+
   return (
     <div className={ "flex w-screen h-screen" }>
-      <div className={ "w-72 " }>
+      <div className={ "w-72" }>
         <Menu />
       </div>
-      <div className="flex-1 flex flex-col">
-        <div className={ "flex-1 relative overflow-y-auto" }>
+      <div className="flex-1 h-screen flex flex-col">
+        <div className={ "w-full h-[calc(100vh-1.5rem)] relative" }>
           <Outlet />
         </div>
         <div className="h-6 my-1">
