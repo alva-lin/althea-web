7efbe6121e69ffaf78dd274a0b7e5aaf5ac639aa
Alva <asjun2lin@outlook.com>
2023-05-11 14:01:55 +0800

    feat: 修改 Token 的设置

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { useLogin } from "./hooks";
 import Router from "./routes";
 
 const App = () => {
   const { isAuthenticated, getAccessToken, isSetToken, setIsSetToken } = useLogin();
+  const [tokenInterceptor, setTokenInterceptor] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     async function initTokenInterceptor() {
       if (isAuthenticated && !isSetToken) {
-        const accessToken = await getAccessToken();
-        axios.interceptors.request.use(function (config) {
-          if (accessToken) {
-            config.headers.Authorization = `Bearer ${ accessToken }`; // 将 token 附加到请求头中
-          }
+        if (tokenInterceptor !== undefined) {
+          axios.interceptors.request.eject(tokenInterceptor);
+        }
+        const interceptor = axios.interceptors.request.use(async function (config) {
+          const accessToken = await getAccessToken();
+          config.headers.Authorization = `Bearer ${ accessToken }`; // 将 token 附加到请求头中
           return config;
         });
+        setTokenInterceptor(interceptor);
         setIsSetToken(true);
       }
     }
 
     initTokenInterceptor().then();
-  }, [ isAuthenticated, getAccessToken, isSetToken, setIsSetToken ]);
+  }, [ isAuthenticated, getAccessToken, isSetToken, setIsSetToken, tokenInterceptor, setTokenInterceptor ]);
 
   return (
     <>
